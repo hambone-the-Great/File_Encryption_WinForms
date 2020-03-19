17f7854cde07add8d0c7125ee0ecd7de59dbde03
Marcus Schuff <marcus.schuff@my.ndsu.edu>
2020-03-19 10:33:18 -0500

    Added progress bar and background worker.

--- File_Encryption_WinForms/FileEncryption.Designer.cs
+++ File_Encryption_WinForms/FileEncryption.Designer.cs
@@ -28,20 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtTarget.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTarget.Enabled = false;
+            this.txtTarget.Location = new System.Drawing.Point(15, 108);
+            this.txtTarget.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(560, 31);
+            this.txtTarget.TabIndex = 0;
+            this.txtTarget.TextChanged += new System.EventHandler(this.TxtTarget_TextChanged);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnEncrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEncrypt.Location = new System.Drawing.Point(386, 181);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(158, 46);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.BtnEncrypt_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(32, 44);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(526, 22);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Drag and Drop a file or folder here to Encrypt or Decrypt";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecrypt.Location = new System.Drawing.Point(67, 181);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(158, 46);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.BtnDecrypt_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pBar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pBar.Location = new System.Drawing.Point(15, 108);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(560, 31);
+            this.pBar.TabIndex = 4;
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
             // 
             // FileEncryption
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(590, 272);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.pBar);
+            this.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
             this.Name = "FileEncryption";
             this.Text = "Windows File Encryption";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FileEncryption_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FileEncryption_DragEnter);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.ComponentModel.BackgroundWorker bgWorker;
     }
 }
 
--- File_Encryption_WinForms/FileEncryption.cs
+++ File_Encryption_WinForms/FileEncryption.cs
@@ -20,52 +20,80 @@ namespace File_Encryption_WinForms
         private bool HasErrors = false;
         private string SuccessLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "File_Encryption_Success.log");
         private string ErrorLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "File_Encryption_Error.log");
+        int FileCount = 0; 
 
+        private Task CurrentTask; 
+        private enum Task
+        {
+            Encrypt = 1, 
+            Decrypt = 2
+        }
 
         public FileEncryption()
         {
             InitializeComponent();
+
+            btnDecrypt.Enabled = false;
+            btnEncrypt.Enabled = false;
+            pBar.Visible = true;
+            pBar.Minimum = 1;
+            pBar.Value = 1;
+            pBar.Step = 1;
+            pBar.Maximum = 100;
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
         }
-
-
-
+        
 
         private void Encrypt(string path)
         {
             FileAttributes attr = File.GetAttributes(path);
+            
+            int i = 0;
 
             if (attr.HasFlag(FileAttributes.Directory))
             {
 
                 foreach (string file in Directory.GetFiles(path))
                 {
-                    try
-                    {
-                        File.Encrypt(file);
-                        Console.WriteLine("Encrypting: " + file);
-                        LogSuccess("encrypted", file);
-                    }
-                    catch (Exception ex)
-                    {
-                        LogError(ex.Message);
-                        Console.WriteLine("Error: " + ex.Message);
-                    }
-                }
-
-                foreach (string subDir in Directory.GetDirectories(path))
-                {
-                    foreach (string file in Directory.GetFiles(subDir))
+                    if (File.Exists(file))
                     {
                         try
                         {
                             File.Encrypt(file);
                             Console.WriteLine("Encrypting: " + file);
-                            LogSuccess("encrypted", file);
+                            LogSuccess("encrypted", file);                            
                         }
                         catch (Exception ex)
                         {
                             LogError(ex.Message);
                             Console.WriteLine("Error: " + ex.Message);
+                        }
+                        UpdateProgress(i++);
+                    }
+                }
+
+                foreach (string subDir in Directory.GetDirectories(path))
+                {
+                    if (Directory.Exists(subDir))
+                    {
+                        foreach (string file in Directory.GetFiles(subDir))
+                        {
+                            if (File.Exists(file))
+                            {
+                                try
+                                {
+                                    File.Encrypt(file);
+                                    Console.WriteLine("Encrypting: " + file);
+                                    LogSuccess("encrypted", file);
+                                }
+                                catch (Exception ex)
+                                {
+                                    LogError(ex.Message);
+                                    Console.WriteLine("Error: " + ex.Message);
+                                }
+                                UpdateProgress(i++);
+                            }
                         }
                     }
                 }
@@ -73,18 +101,25 @@ namespace File_Encryption_WinForms
             }
             else
             {
-                try
+                if (File.Exists(path))
                 {
-                    File.Encrypt(path);
-                    Console.WriteLine("Encrypting: " + path);
-                    LogSuccess("encrypted", path);
-                }
-                catch (Exception ex)
-                {
-                    LogError(ex.Message);
-                    Console.WriteLine("Error: " + ex.Message);
+                    try
+                    {
+                        File.Encrypt(path);
+                        Console.WriteLine("Encrypting: " + path);
+                        LogSuccess("encrypted", path);
+                    }
+                    catch (Exception ex)
+                    {
+                        LogError(ex.Message);
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    UpdateProgress(i++);
                 }
             }
+
+            //percentage = (i + 1) * 100 / FileCount;
+            //bgWorker.ReportProgress(percentage);
         }
 
 
@@ -92,27 +127,14 @@ namespace File_Encryption_WinForms
         {
             FileAttributes attr = File.GetAttributes(path);
 
+            int i = 0;
+
             if (attr.HasFlag(FileAttributes.Directory))
             {
 
                 foreach (string file in Directory.GetFiles(path))
                 {
-                    try
-                    {
-                        File.Decrypt(file);
-                        Console.WriteLine("Decrypting: " + file);
-                        LogSuccess("decrypted", file);
-                    }
-                    catch (Exception ex)
-                    {
-                        LogError(ex.Message);
-                        Console.WriteLine("Error: " + ex.Message);
-                    }
-                }
-
-                foreach (string subDir in Directory.GetDirectories(path))
-                {
-                    foreach (string file in Directory.GetFiles(subDir))
+                    if (File.Exists(file))
                     {
                         try
                         {
@@ -125,26 +147,64 @@ namespace File_Encryption_WinForms
                             LogError(ex.Message);
                             Console.WriteLine("Error: " + ex.Message);
                         }
+                        UpdateProgress(i++);
                     }
                 }
 
+                foreach (string subDir in Directory.GetDirectories(path))
+                {
+                    if (Directory.Exists(subDir))
+                    {
+                        foreach (string file in Directory.GetFiles(subDir))
+                        {
+                            if (File.Exists(file))
+                            {
+                                try
+                                {
+                                    File.Decrypt(file);
+                                    Console.WriteLine("Decrypting: " + file);
+                                    LogSuccess("decrypted", file);
+                                }
+                                catch (Exception ex)
+                                {
+                                    LogError(ex.Message);
+                                    Console.WriteLine("Error: " + ex.Message);
+                                }
+                                UpdateProgress(i++);
+                            }
+                        }
+                    }
+                }
             }
             else
             {
-                try
+
+                if (File.Exists(path))
                 {
-                    File.Decrypt(path);
-                    Console.WriteLine("Decrypting: " + path);
-                    LogSuccess("decrypted", path);
-                }
-                catch (Exception ex)
-                {
-                    LogError(ex.Message);
-                    Console.WriteLine("Error: " + ex.Message);
+                    try
+                    {
+                        File.Decrypt(path);
+                        Console.WriteLine("Decrypting: " + path);
+                        LogSuccess("decrypted", path);
+                        
+
+                    }
+                    catch (Exception ex)
+                    {
+                        LogError(ex.Message);
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    UpdateProgress(i++);
                 }
             }
         }
 
+        private void UpdateProgress(int count)
+        {            
+            double fraction = ((double)(count + 1) / (double)FileCount);
+            int percentage = (int)Math.Round(fraction * 100);
+            bgWorker.ReportProgress(percentage);
+        }
 
         private void LogError(string errorText)
         {
@@ -165,6 +225,74 @@ namespace File_Encryption_WinForms
             }
         }
 
+        private void FileEncryption_DragEnter(object sender, DragEventArgs e)
+        {
+            Console.Write("It is working.");
+            e.Effect = DragDropEffects.Link;
+            
+        }
 
+        private void FileEncryption_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            txtTarget.Text = fileList[0];
+
+            if (Directory.Exists(txtTarget.Text) || File.Exists(txtTarget.Text))
+            {
+                FileCount = Directory.GetFiles(txtTarget.Text, "*.*", SearchOption.AllDirectories).Length;
+            }
+        }
+
+        private void BtnEncrypt_Click(object sender, EventArgs e)
+        {
+            pBar.BringToFront();            
+            CurrentTask = Task.Encrypt;            
+            bgWorker.RunWorkerAsync();
+        }
+
+        private void BtnDecrypt_Click(object sender, EventArgs e)
+        {
+            pBar.BringToFront();            
+            CurrentTask = Task.Decrypt;
+            bgWorker.RunWorkerAsync();
+        }
+
+        private void TxtTarget_TextChanged(object sender, EventArgs e)
+        {
+            if (txtTarget.Text.Length > 1)
+            {
+                btnEncrypt.Enabled = true;
+                btnDecrypt.Enabled = true;
+            }
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (CurrentTask == Task.Encrypt)
+            {
+                Encrypt(txtTarget.Text);
+            }
+            else if (CurrentTask == Task.Decrypt)
+            {
+                Decrypt(txtTarget.Text);
+            }
+            else
+            {
+                MessageBox.Show("Someting went wrong.");
+            }
+        }
+
+        private void BgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pBar.Value = e.ProgressPercentage;
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Process.Start(SuccessLog);
+            if (HasErrors) Process.Start(ErrorLog);            
+            pBar.SendToBack();
+            txtTarget.Text = string.Empty;
+        }
     }
 }
